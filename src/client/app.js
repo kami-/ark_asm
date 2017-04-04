d4b62147f277eba04f4248867fb79994624ea680
László Major <laszlo.major@outlook.com>
2017-04-04 21:55:44 +0200

    WIP graph rendering and updating.

--- src/client/app.js
+++ src/client/app.js
@@ -1,7 +1,65 @@
-(function() {
-    const webSocket = new WebSocket('ws://localhost:8084');
+const Rickshaw = require("Rickshaw");
 
-    webSocket.addEventListener('message', function (event) {
-        console.log('Message from server', JSON.parse(event.data));
+const seriesAxes = ["fps", "fpsMin", "cps", "playerCount", "localAiCount", "remoteAiCount", "entityCount"];
+const missions = {};
+const webSocket = new WebSocket('ws://localhost:8084');
+
+webSocket.addEventListener('message', function (event) {
+    var rawMission = JSON.parse(event.data);
+    console.log('Message from server', rawMission);
+    processMission(rawMission);
+});
+
+function processMission(rawMission) {
+    var mission = missions[rawMission.missionId];
+    if (!mission) {
+        mission = createMission(rawMission);
+        missions[rawMission.missionId] = mission;
+        mission.graph = createGraph(mission);
+        console.log("graph created", mission.graph);
+    }
+    console.log(mission);
+    pushSeriesData(mission, rawMission);
+    mission.graph.update();
+}
+
+function createMission(rawMission) {
+    return {
+        missionId: rawMission.missionId,
+        seriesData: seriesAxes.reduce((acc, prop) => {
+            acc[prop] = [];
+            return acc;
+        }, {})
+    };
+}
+
+function pushSeriesData(mission, rawMission) {
+    const toPointWithTickTime = toPoint.bind(null, rawMission.tickTime);
+    seriesAxes.forEach(prop => {
+        mission.seriesData[prop].push(toPointWithTickTime(rawMission[prop]));
     });
-})();
+}
+
+function toPoint(tickTime, y) {
+    return { x: tickTime, y: y };
+}
+
+function createGraph(mission) {
+    const palette = new Rickshaw.Color.Palette( { scheme: 'classic9' } );
+    return new Rickshaw.Graph( {
+        element: document.getElementById("chart"),
+        width: 900,
+        height: 500,
+        renderer: "line",
+        stroke: true,
+        preserve: true,
+        series: seriesAxes.map(prop => {
+            console.log(prop, mission.seriesData[prop]);
+            return {
+                color: palette.color(),
+                data: mission.seriesData[prop],
+                name: prop
+            };
+        })
+    });
+}
