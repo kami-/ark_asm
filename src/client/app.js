fb97ec2e5ee44e9e1d901fdb2297150cde7711f2
László Major <laszlo.major@outlook.com>
2017-04-06 22:52:50 +0200

    WIP chart experimenting. Using pure d3 instead of rickshaw.

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,38 @@
-const Rickshaw = require("Rickshaw");
+const d3 = require("d3");
 
-const seriesAxes = ["fps", "fpsMin", "cps", "playerCount", "localAiCount", "remoteAiCount", "entityCount"];
+const seriesConfig = {
+    fps: {
+        label: "FPS",
+        maxValue: 70
+    },
+    fpsMin: {
+        label: "FPS Min",
+        maxValue: 70
+    },
+    cps: {
+        label: "CPS",
+        maxValue: 70
+    },
+    playerCount: {
+        label: "Players",
+        maxValue: 400
+    },
+    localAiCount: {
+        label: "Local AI",
+        maxValue: 1000
+    },
+    remoteAiCount: {
+        label: "Remote AI",
+        maxValue: 1000
+    },
+    entityCount: {
+        label: "Entities",
+        maxValue: 1000
+    }
+};
+const seriesAxes = Object.getOwnPropertyNames(seriesConfig);
+
+const shownSeconds = 300;
 const missions = {};
 const webSocket = new WebSocket('ws://localhost:8084');
 
@@ -15,12 +47,12 @@ function processMission(rawMission) {
     if (!mission) {
         mission = createMission(rawMission);
         missions[rawMission.missionId] = mission;
-        mission.graph = createGraph(mission);
+        createGraph(mission);
         console.log("graph created", mission.graph);
     }
     console.log(mission);
     pushSeriesData(mission, rawMission);
-    mission.graph.update();
+    updateGraph(mission);
 }
 
 function createMission(rawMission) {
@@ -44,22 +76,85 @@ function toPoint(tickTime, y) {
     return { x: tickTime, y: y };
 }
 
-function createGraph(mission) {
-    const palette = new Rickshaw.Color.Palette( { scheme: 'classic9' } );
-    return new Rickshaw.Graph( {
-        element: document.getElementById("chart"),
-        width: 900,
-        height: 500,
-        renderer: "line",
-        stroke: true,
-        preserve: true,
-        series: seriesAxes.map(prop => {
-            console.log(prop, mission.seriesData[prop]);
-            return {
-                color: palette.color(),
-                data: mission.seriesData[prop],
-                name: prop
-            };
-        })
+var tm = 0;
+setInterval(() => {
+    var rawMission = {
+        tickTime: tm,
+        missionId: "123"
+    };
+    seriesAxes.forEach(prop => {
+        rawMission[prop] = Math.random() * seriesConfig[prop].maxValue;
     });
+    tm = tm + 5;
+    console.log(tm, "generated rawMission", rawMission);
+    processMission(rawMission);
+}, 5000);
+
+function createGraph(mission) {
+    const width = 900,
+        height = 500;
+
+    const x = d3.scale.linear()
+        .domain([0, shownSeconds])
+        .range([0, width]);
+
+    const y = d3.scale.linear()
+        .domain([0, 1000])
+        .range([height, 0]);
+
+    const line = d3.svg.line()
+        //.interpolate('basis')
+        .x(d => x(d.x))
+        .y(d => y(d.y));
+
+    const svg = d3.select("#missions > .graph").append("svg")
+        .attr("class", "chart")
+        .attr("width", width)
+        .attr("height", height + 50)
+
+    const xAxis = d3.svg.axis()
+        .tickFormat(d => "" + Math.floor(d/60) + "m")
+        .scale(x)
+        .orient("bottom");
+
+    const axis = svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+
+    const paths = svg.append("g")
+
+    mission.seriesPaths = {};
+    seriesAxes.forEach(prop => {
+        const path = paths.append("path")
+            .data([mission.seriesData[prop]])
+            .attr("class", prop + " group")
+            .style("stroke", "black");
+        mission.seriesPaths[prop] = path;
+    });
+
+    mission.graph = {
+        x: x,
+        y: y,
+        line: line,
+        axis: axis,
+        xAxis: xAxis,
+        paths: paths
+    };
+}
+
+function updateGraph(mission) {
+    seriesAxes.forEach(prop => {
+        mission.seriesPaths[prop]
+            .attr("d", mission.graph.line);
+    });
+
+    if (tm >= shownSeconds) {
+        mission.graph.x.domain([tm - shownSeconds, tm]);
+    }
+
+    mission.graph.axis.transition()
+        .duration(1)
+        .ease("linear")
+        .call(mission.graph.xAxis);
 }
